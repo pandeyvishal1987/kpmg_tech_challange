c8475f07ac98930c362e2229523c32693938832d
vishal.pandey <vishal.pandey@rakuten.com>
2022-04-14 18:46:30 +0530

    updated function name

--- nested_object/nested_object.py
+++ nested_object/nested_object.py
@@ -1,5 +1,5 @@
 from functools import reduce
-def deep_get(dictionary,keys, default=None):
+def nested_object(dictionary,keys, default=None):
     a = reduce(lambda z, key: z.get(key, default) if isinstance(z, dict) else default, keys.split("/"), dictionary)
     if isinstance(a, str):
         return a
